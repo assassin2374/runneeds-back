79560a837a5c013a066a46c583808a5f81e86e31
assassin_box <assassin543@outlook.jp>
2021-06-16 23:46:09 +0900

    postまで実装

--- docker/postgres/init/init.sql
+++ docker/postgres/init/init.sql
@@ -2,7 +2,7 @@ SET client_encoding = 'UTF8';
 
 CREATE TABLE users (
   id SERIAL NOT NULL,
-  user_name varchar(20) NOT NULL,
+  name varchar(20) NOT NULL,
   email varchar(64) NOT NULL,
   pass varchar(20) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
@@ -10,7 +10,7 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
-INSERT INTO users (user_name, email, pass) VALUES
+INSERT INTO users (name, email, pass) VALUES
 ('sam', 'sample01@example.com', 'sample01'),
 ('tom', 'sample02@example.com', 'sample02'),
 ('jerry', 'sample03@example.com', 'sample03')
--- src/main.ts
+++ src/main.ts
@@ -32,31 +32,123 @@ const pgConnect = async () => {
 // pgConnect();
 
 type User = {
-  id: number;
-  user_name: string;
+  id?: number;
+  name: string;
   email: string;
   pass: string;
 };
 
-// API作成
+// getAll作成
 app.get("/", async (req, res) => {
   // SQLクエリ実行
-  const sqlQuery = "SELECT * FROM users";
+  const sqlQuery = {
+    text: "SELECT * FROM users",
+  };
   const client = await pgConnect();
   const result = await client.query<User>(sqlQuery);
-  // User型に格納（created_at、updated_atが残るエラーの）
+  // User型に格納（created_at、updated_atが残るエラー）
   const users = result.rows.map((user) => {
     return {
       id: user.id,
-      user_name: user.user_name,
+      name: user.name,
       email: user.email,
       pass: user.pass,
     } as User;
   });
   console.log(users);
 
-  res.status(200).send(users);
+  res.status(200).json(users);
 });
+
+// get作成
+app.get("/:id", async (req, res) => {
+  // id取得
+  const id = parseInt(req.params.id);
+  // SQLクエリ実行
+  const sqlQuery = {
+    text: "SELECT * FROM users WHERE id=($1)",
+    values: [id],
+  };
+  const client = await pgConnect();
+  const result = await client.query<User>(sqlQuery);
+  // User型に格納（created_at、updated_atが残るエラー）
+  const user = {
+    id: result.rows[0].id,
+    name: result.rows[0].name,
+    email: result.rows[0].email,
+    pass: result.rows[0].pass,
+  };
+
+  console.log(user);
+
+  res.status(200).json(user);
+});
+
+// post作成
+app.post("/", async (req, res) => {
+  const user = req.body as User;
+  delete user.id;
+  // SQLクエリ実行
+  const sqlQuery = {
+    text: `
+      INSERT INTO 
+        users (name, email, pass) 
+      VALUES 
+        ($1, $2, $3)
+      RETURNING id
+      `,
+    values: [user.name, user.email, user.pass],
+  };
+  const client = await pgConnect();
+  const result = await client.query<{ id: number }>(sqlQuery);
+
+  const id = result.rows[0].id;
+  console.log(id);
+
+  // sendは文字列しか送れないため
+  res.status(200).json(id);
+});
+
+// // put作成
+// app.put("/:id", async (req, res) => {
+//   // id取得
+//   const id = parseInt(req.params.id);
+//   const date = req.body;
+
+//   // SQLクエリ実行
+//   const sqlQuery = {
+//     text: "UPDATE users SET ($1) WHERE id=($2)",
+//     values: [date, id],
+//   };
+//   const client = await pgConnect();
+//   const result = await client.query<User>(sqlQuery);
+//   // User型に格納（created_at、updated_atが残るエラー）
+//   const user = result.rows[0];
+
+//   console.log(user);
+
+//   res.status(200).json(user);
+// });
+
+// // delete作成
+// app.delete("/:id", async (req, res) => {
+//   // id取得
+//   const id = parseInt(req.params.id);
+
+//   // SQLクエリ実行
+//   const sqlQuery = {
+//     text: "DELETE FROM user where id=($1)",
+//     values: [id],
+//   };
+//   const client = await pgConnect();
+//   const result = await client.query<User>(sqlQuery);
+//   // User型に格納（created_at、updated_atが残るエラーの）
+//   const user = result.rows[0];
+
+//   console.log(user);
+
+//   res.status(200).json(user);
+// });
 
 // Webサーバ起動
 app.listen(port);
