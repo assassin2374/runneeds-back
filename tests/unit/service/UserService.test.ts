2af9ea0d7667ce1012cdd6f0badd34a7bef6ce40
assassin_box <assassin543@outlook.jp>
2021-09-08 06:09:23 +0900

    getAllテスト正常系作成

--- tests/unit/service/UserService.test.ts
+++ tests/unit/service/UserService.test.ts
@@ -3,44 +3,6 @@ import { User } from "../../../src/model/User";
 import { UserService } from "../../../src/service/user/UserService";
 import { HttpStatusCode } from "../../../src/model/utils/HttpStatusCode";
 
-const mockUsers: User[] = [
-  {
-    id: 1,
-    name: "name1",
-    email: "email1",
-    pass: "pass1",
-  },
-  {
-    id: 2,
-    name: "name2",
-    email: "email2",
-    pass: "pass2",
-  },
-  {
-    id: 3,
-    name: "name3",
-    email: "email3",
-    pass: "pass3",
-  },
-];
-
-const mockUser: User = {
-  id: 1,
-  name: "name1",
-  email: "email1",
-  pass: "pass1",
-};
-
-const creatMockUsers = (id: number) => {
-  const mockUser: User = {
-    id: id,
-    name: `name_${id}`,
-    email: `email_${id}`,
-    pass: `pass_${id}`,
-  };
-  return mockUser;
-};
-
 const mockRepository: IUserRepository = {
   getAll(): Promise<User[]> {
     return new Promise<User[]>(function () {
@@ -71,6 +33,7 @@ const mockRepository: IUserRepository = {
 
 describe("userAPI getテスト", () => {
   it("get、正常系(1件突っ込み、1件取得)", async () => {
+    // test用のモック関数作成
     const getfunc = (id: number): Promise<User> => {
       return new Promise<User>(function (resolve) {
         resolve(mockUser);
@@ -95,6 +58,7 @@ describe("userAPI getテスト", () => {
     expect(mockUser.pass).toBe(resultUser.pass);
   });
   it("get、異常系(リクエストしたidが存在しない場合、ステータスコードを見る)", async () => {
+    // test用のモック関数作成
     const getfunc = (id: number): Promise<User> => {
       return new Promise<User>(function (resolve) {
         const object = {};
@@ -118,27 +82,77 @@ describe("userAPI getテスト", () => {
 
 describe("userAPI getAllテスト", () => {
   it("getAll、正常系(3件突っ込み、3件取得)", async () => {
-    const getfunc = (id: number): Promise<User[]> => {
+    // テスト用引数
+    const testNum = 3;
+    const MockUsers = creatMockUsers(testNum);
+    // test用のモック関数作成
+    const getfunc = (): Promise<User[]> => {
       return new Promise<User[]>(function (resolve) {
-        resolve(creatMockUsers(3));
+        resolve(MockUsers);
       });
     };
     mockRepository.getAll = getfunc;
-    // idはnumber型で固定（null回避）
-    const id = 0;
     const userService = new UserService(mockRepository);
 
     // user一件取得
-    const result = await userService.getAll;
+    const result = await userService.getAll();
 
     //ステータスのチェック
     expect(HttpStatusCode.OK).toBe(result.statusCode);
 
-    const resultUser = result.value as User;
+    const userList = result.value as User[];
     // 項目の検証
-    expect(mockUser.id).toBe(resultUser.id);
-    expect(mockUser.name).toBe(resultUser.name);
-    expect(mockUser.email).toBe(resultUser.email);
-    expect(mockUser.pass).toBe(resultUser.pass);
+    expect(testNum).toBe(userList.length);
+
+    const userMap = createUserMap(userList);
+    MockUsers.forEach((createdUser) => {
+      const id = createdUser.id as number;
+      const user = userMap.get(id) as User;
+      expect(createdUser.id).toBe(user.id);
+      expect(createdUser.name).toBe(user.name);
+      expect(createdUser.email).toBe(user.email);
+      expect(createdUser.pass).toBe(user.pass);
+    });
   });
 });
+
+// 単一のUserデータ
+const mockUser: User = {
+  id: 1,
+  name: "name1",
+  email: "email1",
+  pass: "pass1",
+};
+
+/**
+ * 複数のUserデータを作成する
+ * @returns 作成されたUserListデータ
+ */
+const creatMockUsers = (num: number) => {
+  const mockUsers: User[] = [];
+  for (let index = 0; index < num; index++) {
+    const user: User = {
+      id: index,
+      name: `name_${index}`,
+      email: `email_${index}`,
+      pass: `pass_${index}`,
+    };
+    mockUsers.push(user);
+  }
+  return mockUsers;
+};
+
+/**
+ * UserデータからidをキーとするMapを作成する
+ * @returns 作成されたUserMapデータ
+ */
+function createUserMap(users: User[]): Map<number, User> {
+  const result = new Map<number, User>();
+
+  users.forEach((user) => {
+    const userId = user.id as number;
+    result.set(userId, user);
+  });
+
+  return result;
+}
